8e0b52b5f165c71faf6bbf9f976735810f819f2c
graemefoster <graemefoster@microsoft.com>
2022-07-20 15:48:06 +0800

    Adding sample java api

---
+++ src/main/java/com/graemefoster/appservicejavaapi/AppservicejavaapiApplication.java
@@ -0,0 +1,13 @@
+package com.graemefoster.appservicejavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppservicejavaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppservicejavaapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graemefoster/appservicejavaapi/AppservicejavaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graemefoster.appservicejavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppservicejavaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
