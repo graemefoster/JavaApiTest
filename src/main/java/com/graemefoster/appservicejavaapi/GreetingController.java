f2ff4ac8636a7f68469c821b0fd153cede943a29
graemefoster <graemefoster@microsoft.com>
2022-10-26 09:20:26 +0800

    Small updates

--- src/main/java/com/graemefoster/appservicejavaapi/GreetingController.java
+++ src/main/java/com/graemefoster/appservicejavaapi/GreetingController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 @RestController
 public class GreetingController {
 
@@ -16,10 +22,17 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public HttpEntity<Greeting> greeting(
-            @RequestParam(value = "name", defaultValue = "World") String name) {
+            @RequestParam(value = "name", defaultValue = "World") String name) throws IOException {
+
+        //reach out to another page to show
+        URL url = new URL("https://www.google.com");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int response = con.getResponseCode();
 
         Greeting greeting = new Greeting(String.format(TEMPLATE, name));
-        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name + ":" + response + ":" + System.getenv("WEBSITE_HOSTNAME"))).withSelfRel());
+
 
         return new ResponseEntity<>(greeting, HttpStatus.OK);
     }
